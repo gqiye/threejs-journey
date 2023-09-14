4af6202f9ab1c560e2c6c5585dab15fa262ab727
602501592@qq.com <602501592@qq.com>
2023-09-14 10:24:54 +0800

    使用三种光照实现阴影

--- 16-shadows/src/script.js
+++ 16-shadows/src/script.js
@@ -18,14 +18,19 @@ const scene = new THREE.Scene()
 
 /**
  * Lights
+ * 只有三种灯光支持投阴影
+ * pointLight DirectionalLight spotLight 
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
+// console.time('directionalLight')
 
-// Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+/**
+ *  Directional light
+ */
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
 directionalLight.position.set(2, 2, - 1)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
@@ -33,6 +38,89 @@ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
+
+// 设置灯光能补获 阴影
+directionalLight.castShadow =true;
+
+// 阴影保存在灯光的shadow
+console.log("directionalLight.shadow", directionalLight.shadow)
+
+// 修改阴影地图的分辨率 2的幂 
+directionalLight.shadow.mapSize.width =1024
+directionalLight.shadow.mapSize.height = 1024
+// 阴影相机位置 调整一个小的渲染范围,减少gpu渲染
+// 缩小渲染范围,会使渲染效果更好
+// 因为一个渲染地图太大会导致里面小的物体变小分辨率
+// 如果小的地图和小的物体无疑会渲染效果更好
+directionalLight.shadow.camera.top=2
+directionalLight.shadow.camera.right=2
+directionalLight.shadow.camera.bottom=-2
+directionalLight.shadow.camera.left=-2
+// 阴影相机 相机距离物体的远近
+// 缩小渲染范围,会使渲染效果更好
+directionalLight.shadow.camera.near =1 
+directionalLight.shadow.camera.far =6
+
+// 阴影模糊效果
+directionalLight.shadow.radius = 10; 
+
+
+// 新建一个阴影相机助手 也是基于灯光的位置
+const directionalLightCamerHelper = new THREE.CameraHelper(
+    directionalLight.shadow.camera
+)
+// 隐藏助手
+directionalLightCamerHelper.visible = false
+scene.add(directionalLightCamerHelper)
+
+
+/**
+ * spot light
+ */
+const spotLight = new THREE.SpotLight(
+    0xffffff,0.4,10,Math.PI*0.3
+)
+spotLight.castShadow= true;
+// 优化阴影效果
+spotLight.shadow.mapSize.width =1024;
+spotLight.shadow.mapSize.height =1024;
+// 阴影视场变小
+spotLight.shadow.camera.fov = 30
+spotLight.shadow.camera.near =1
+spotLight.shadow.camera.far = 6
+
+spotLight.position.set(0,2,2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
+// 助手
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+spotLightCameraHelper.visible = false
+scene.add(spotLightCameraHelper)
+
+/**
+ * point Light
+ */
+const pointLight = new THREE.PointLight(0xffffff,0.3);
+pointLight.castShadow = true;
+// 优化阴影
+pointLight.shadow.mapSize.width =1024;
+pointLight.shadow.mapSize.height =1024;
+pointLight.shadow.camera.near =1
+pointLight.shadow.camera.far = 6
+
+// 照射方向有点像透视相机的,如果是四面八方的照射,会导致一些事故
+pointLight.position.set(-1,1,0);
+scene.add(pointLight)
+
+// 增加助手
+const pointLightCameraHelper = new THREE.CameraHelper(
+    pointLight.shadow.camera
+)
+pointLightCameraHelper.visible = false
+scene.add(pointLightCameraHelper)
+
+// console.timeEnd('directionalLight')
 /**
  * Materials
  */
@@ -48,6 +136,8 @@ const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
+// 设置球体可以投下阴影
+sphere.castShadow =true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
@@ -55,6 +145,9 @@ const plane = new THREE.Mesh(
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
+// 设置平面可以接收阴影
+plane.receiveShadow =true
+
 
 scene.add(sphere, plane)
 
@@ -104,7 +197,16 @@ const renderer = new THREE.WebGLRenderer({
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
- 
+
+// 渲染器阴影开启
+renderer.shadowMap.enabled =true
+// 阴影渲染算法选择
+// 边缘好看
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+// 有模糊效果
+// renderer.shadowMap.type = THREE.PCFShadowMap
+
 
 /**
  * Animate
