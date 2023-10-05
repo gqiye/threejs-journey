ccf39762253d3ea4d153dae083a7172854779d54
602501592@qq.com <602501592@qq.com>
2023-10-05 22:17:22 +0800

    头盔上的点根据头盔视角转动进行显示隐藏

--- 36-mixing-html-and-webgl/src/script.js
+++ 36-mixing-html-and-webgl/src/script.js
@@ -3,14 +3,46 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { gsap } from 'gsap'
 
+// 位置不知道怎么确定`的
+// position：用来存放3D位置坐标
+// element: 从HTML获取对应点元素的节点
+// https://blog.csdn.net/weixin_40856652/article/details/125302743
+
+const points = [
+    {
+        position: new THREE.Vector3(1.55, 0.3, - 0.6),
+        element: document.querySelector('.point-0')
+    },
+    {
+        position: new THREE.Vector3(0.5, 0.8, - 1.6),
+        element: document.querySelector('.point-1')
+    },
+    {
+        position: new THREE.Vector3(1.6, - 1.3, - 0.7),
+        element: document.querySelector('.point-2')
+    }
+]
+
+
+// 光线投射
+// https://threejs.org/docs/#api/zh/core/Raycaster
+const raycaster = new THREE.Raycaster()
+
 /**
  * Loaders
  */
+let sceneReady = false
+
 const loadingBarElement = document.querySelector('.loading-bar')
 const loadingManager = new THREE.LoadingManager(
     // Loaded
     () =>
     {
+
+        window.setTimeout(() =>
+        {
+            sceneReady = true
+        }, 2000)
         // Wait a little
         window.setTimeout(() =>
         {
@@ -20,6 +52,8 @@ const loadingManager = new THREE.LoadingManager(
             // Update loadingBarElement
             loadingBarElement.classList.add('ended')
             loadingBarElement.style.transform = ''
+
+            // sceneReady = true
         }, 500)
     },
 
@@ -193,8 +227,80 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const tick = () =>
 {
+    
     // Update controls
     controls.update()
+    if(sceneReady)
+    {
+    // 遍历每个点
+    for(const point of points)
+    {
+        // 根据场景中点的3D坐标来获取点在屏幕上的二维坐标。
+        // 使用clone()方法克隆点的位置得到screenPosition
+        // 让点跟随头盔移动
+        const screenPosition = point.position.clone();
+        // 对screenPosition调用project(...) 方法并传入摄像机camera作为参数
+        // 该点的x坐标，可以看到其值非常接近于0。按住右键把头盔拖往屏幕左侧，值将接近-1，拖往屏幕右侧，值将接近+1。
+        screenPosition.project(camera)
+
+        // 要让射线从摄像机发射到对应点，需要调用setFromCamera(...)方法
+        // 第一个参数是对应点在屏幕上的二维坐标，
+        // 第二个参数则是射线来源的相机。
+        // screenPosition是一个Vector3，但是我们只用到他的x和y属性
+        raycaster.setFromCamera(screenPosition, camera)
+        // intersectObject(...)方法来检查与射线相交的物体
+        // 第一个是scene.children，场景中所有物体
+        // 第二个参数为true代表开启递归测试，会递归遍历物体的子代。
+        // intersectObjects(...)方法会返回一个相交对象数组，这些对象会按照距离排序
+        // console.log(scene.children)
+        const intersects = raycaster.intersectObjects(scene.children, true)
+
+        /**
+         * 判断点是否显示
+         * 从摄像机发射一条射线到对应点，如果没有东西与射线相交，则显示该点,
+         * 若有东西与射线相交，则计算与该交点的距离，如果交点比点元素距离更远，
+         * 则说明点元素在交点前面，继续显示，
+         * 如果交点离摄像机距离更近，则说明点元素被覆盖住了，要移除visible样式将点给隐藏起来。
+         * 先判断射线是否有相交物体，没有的话则添加visible类显示该点，若有相交物体，再做后续其他判断
+         */
+        // 先判断射线是否有相交物体，没有的话则添加visible类显示该点，若有相交物体，再做后续其他判断
+        if(intersects.length === 0)
+        {
+            point.element.classList.add('visible')
+        }
+        else
+        {
+            // intersectObjects(...)方法会返回一个相交对象数组，这些对象会按照距离排序，
+            // 这也就意味着我们无需去测试全部相交对象，只需要测试第一个便可，
+            // 通过distance属性获取距离值。
+            // 然后通过distanceTo(...)方法传入相机位置，获取点元素到相机的距离值。
+            // 接着比较两个值大小，决定点元素的显隐：
+
+            // 物体到相机距离
+            const intersectionDistance = intersects[0].distance
+            // 点到相机距离
+            const pointDistance = point.position.distanceTo(camera.position)
+			if(intersectionDistance < pointDistance)
+            {
+                point.element.classList.remove('visible')
+            }
+            else
+            {
+                point.element.classList.add('visible')
+            }
+        }
+
+
+        //接下去要把这个坐标转换成像素值，因为要根据像素值去改变页面元素的位置。
+        // 要把场景中的点坐标转换为屏幕中的像素，需要乘以渲染器尺寸的一半：
+        const translateX = screenPosition.x * sizes.width * 0.5
+        // y轴下方代表正值，而在Three.js中则是普通的坐标系y轴上方代表正值，因此translateY要取反
+        const translateY = - screenPosition.y * sizes.height * 0.5
+        // 更新元素的transform属性，在x轴上平移了translateX个像素,y轴同理，
+        point.element.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`        
+        console.log(translateX)
+    }
+}
 
     // Render
     renderer.render(scene, camera)
